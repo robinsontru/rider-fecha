de3ac8438f546b14f26a9be647d1ff93d1fa549c
robinsontru <109121630+robinsontru@users.noreply.github.com>
2022-11-02 10:11:55 -0500

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,20 @@
+//{}
+
+function datos(nombre,Apellido,cc,fecha)  {
+document.getElementById('nombre').innerHTML=nombre
+document.getElementById('Apellido').innerHTML=Apellido
+document.getElementById('cc').innerHTML=cc
+
+alert(nombre+""+Apellido+""+""+cc+""+fecha)
+// function edad(fecha)
+// var difirencia = Date.now() - fecha.getTime();
+
+// fecha = new Date(difirencia);
+
+// return Math.abs(fecha.getUTCFullYear() - 2004);
+
+}
+// console.log(edad(new Date(2004)))
+
+
+
